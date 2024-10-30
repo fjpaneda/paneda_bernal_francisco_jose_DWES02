ec0d0db8b74ed83c9427b5562f67e21ace22111b
fjpaneda <fjpaneda@birt.eus>
2024-10-30 21:28:14 +0100

    test

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
-    require "recursos.php";
+    require "lib/recursos.php";
 
-    if (isset($_POST['nombre']) && isset($_POST['apellidos'])){
-        //nombre y apellidos rellenados en el formulario
+    if (isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['dni'])){
+        //nombre y apellido rellenados en el formulario
+        $nombre_usuario = $_POST['nombre'];
+        $apellido_usuario = $_POST['apellido'];
+        $dni_usuario = $_POST['dni'];
+        $usuario_valido = false;
+        foreach (USUARIOS as $usuario){
+            if ($nombre_usuario == $usuario['nombre'] && $apellido_usuario == $usuario['apellido'] && $dni_usuario == $usuario['dni']){
+                $usuario_valido = true;
+                break;
+            }
+        }
     }
+    
 
     if (isset($_POST['dni'])){
         //comprobar que el dni es correcto
     }
     if (isset($_POST['modelo'])){
+        $modelo_elegido = $_POST['modelo'];
         //comprobar que el modelo esta seleccionado
+        foreach ($coches as $coche) {
+            if ($modelo_elegido == $coche['modelo']){
+                if($coche['disponible']){
+                    $coche['disponible'] = false;
+                    $coche['fecha_inicio'] = $_POST['fecha'];
+                    $coche['fecha_fin'] = $_POST['fecha'] + $_POST['duracion'];
+                }
+
+            }
+        }
+
     }
 
     if (isset($_POST['fecha'])){
@@ -18,6 +41,11 @@
 
     if (isset($_POST['duracion'])){
         //comprobar que el valor es >=1 y <=30
+        $duracion_valida = true;
+        $dias = $_POST['duracion'];
+        if ($dias < 1 || $dias >30){
+            $duracion_valida = false;
+        }
     }
 ?>
 
@@ -30,6 +58,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        
     </head>
     
     <body>
@@ -37,8 +69,8 @@
             <label for="nombre">Nombre: </label>
             <input type="text" name="nombre" id="nombre" placeholder="Nombre del usuario">
             <br>
-            <label for="apellidos">Apellidos: </label>
-            <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos del usuario">
+            <label for="apellido">Apellido: </label>
+            <input type="text" name="apellido" id="apellido" placeholder="Apellido del usuario">
             <br>
             <label for="dni">DNI: </label>
             <input type="text" name="dni" id="dni" placeholder="12345678A">
